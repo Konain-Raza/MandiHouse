5a5bb6f062929369ebe5d13b3c5c043245303a33
Konain <konainraza105@gmail.com>
2024-09-30 22:24:30 +0500

    added video and update data.js fil

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import video from "../assets/videos/mandihouse.mp4"
 
 const Video = () => {
   return (
     <div className="h-max rounded-3xl w-full flex items-center justify-center mx-auto overflow-hidden">
       <video
         className="w-[90%] rounded-3xl aspect-video"
-        src="../../public/assets/videos/mandihouse.mp4" // Replace with your direct video URL (e.g., a .mp4 file)
+        src={video} 
         title="Video"
         autoPlay
         loop
