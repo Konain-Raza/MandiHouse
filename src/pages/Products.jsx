21377c56c99c2575d68c2814de0d4fac16eee720
Konain Raza <konainraza105.com>
2024-10-02 09:18:33 +0500

    fixed width of products wrapper

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -8,19 +8,21 @@ const Products = () => {
   const [currentProduct, setCurrentProduct] = useState([]);
 
   return (
-    <div className="relative flex flex-row flex-wrap mx-auto g">
-      <Video/>
-      {ProductData.map((products,index) => {
+    <div className="relative w-full justify-between lg:px-10 flex flex-row flex-wrap mx-auto g">
+      <Video />
+      {ProductData.map((products, index) => {
         return (
           <ProductCard
-          key={products.id}
+            key={products.id}
             data={products}
             setIsModal={setIsModal}
             setCurrentProduct={setCurrentProduct}
           />
         );
       })}
-      {isModal && <ProductModal onClose={setIsModal} currentProduct={currentProduct} />}
+      {isModal && (
+        <ProductModal onClose={setIsModal} currentProduct={currentProduct} />
+      )}
     </div>
   );
 };
